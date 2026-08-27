a8d699586ad86fa1ed7384ca067c51fd07d51cf0
tdebug <tdebug@users.noreply.github.com>
2026-08-27 16:03:01 +0300

    Display Pandas DataFrame in Streamlit app
    Convert Snowpark DataFrame to Pandas DataFrame for display.

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,6 +20,11 @@ session = cnx.session()
 #session = get_active_session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
+#st.stop()
+
+#convert the snowpark DF to Pandas DF to use LOC function
+pd_df=my_dataframe.to_pandas()
+st.dataframe(pd_df)
 st.stop()
 
 ingredients_list = st.multiselect(
